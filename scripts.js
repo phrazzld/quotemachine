75439ab9eac76ee36f2f2736ea860757b444e7ee
Phaedrus <phraznikov@gmail.com>
2017-02-07 15:51:03 -0800

    Get Twitter widget styled and autofilling data-text

--- scripts.js
+++ scripts.js
@@ -3,8 +3,24 @@ var quoteauthor = document.getElementById("author");
 
 function nextQuote() {
   var quote = quotes[Math.floor(Math.random() * quotes.length)];
-  quotecontent.innerHTML = quote.content;
+  quotecontent.innerHTML = "\"" + quote.content + "\"";
   quoteauthor.innerHTML = quote.author;
+  obliterateTwitterLink();
+  twttr.ready(function() {
+    twttr.widgets.createShareButton(
+        '/',
+        document.getElementById('twitterlink-wrapper'),
+        {
+          text: "\"" + quote.content + "\"" + " --" + quote.author,
+          size: "large"
+        }
+        );
+  });
+}
+
+function obliterateTwitterLink() {
+  var twitterlink = document.getElementById("twitterlink-wrapper");
+  twitterlink.innerHTML = "";
 }
 
 var button = document.getElementById("next-quote");
